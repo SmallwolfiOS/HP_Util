3e3adb74aca2411d996f176eb7e61a3b523c8fee
Jason's iMac <1348748408@qq.com>
2016-05-09 16:38:04 +0800

    add demo

---
+++ HP_UtilDemo/HP_Util.h
@@ -0,0 +1,17 @@
+//
+//  HP_Util.h
+//  dsadas
+//
+//  Created by Jason on 16/5/9.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import <UIKit/UIKit.h>
+
+@interface HP_Util : NSObject
+
+// 使用UIColor创建UIImage
++ (UIImage *) createImageWithColor: (UIColor *)color;
+
+@end
---
+++ HP_UtilDemo/HP_UtilDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HP_UtilDemo
+//
+//  Created by Jason on 16/5/9.
+//  Copyright © 2016年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
